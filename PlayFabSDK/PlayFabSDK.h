f126f0ca419ba28b0d9cbaf9d7105f5655322610
Thiago Amendola <amendolathiago@gmail.com>
2019-08-19 16:25:47 -0300

    First commit

---
+++ PlayFabSDK/PlayFabSDK.h
@@ -0,0 +1,20 @@
+//
+//  PlayFabSDK.h
+//  PlayFabSDK
+//
+//  Created by Jackdaw on 2/3/16.
+//  Copyright © 2016 PlayFab, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <PlayFabSDK/PlayFabClientAPI.h>
+#import <PlayFabSDK/PlayFabSettings.h>
+#import <PlayFabSDK/PlayFabVersion.h>
+#import <PlayFabSDK/PlayFabBaseModel.h>
+#import <PlayFabSDK/PlayFabClientDataModels.h>
+#import <PlayFabSDK/PlayFabError.h>
+
+@interface PlayFabSDK : NSObject
+
+@end
